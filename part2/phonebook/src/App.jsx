198c7e7457ab96b6dbdd91556228abac216dbbcb
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-12-03 13:09:27 +0100

    Phonebook solution 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,15 +3,20 @@ import { useState } from 'react'
 
 function App() {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: "+421123456789"
+
+     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const person = {
       name: newName,
+      number: newNumber
     }
 
     if(!(persons.find((element) => element.name === person.name))){
@@ -28,12 +33,19 @@ function App() {
     setNewName(event.target.value)
   }
 
+  const handleInputNumChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleInputChange} />
+          name: <input value={newName} onChange={handleInputChange} required />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleInputNumChange} required />
         </div>
         <div>
           <button type="submit" >add</button>
@@ -42,7 +54,7 @@ function App() {
       <h2>Numbers</h2>
       {persons.map((person, i) => {
         return(
-          <p key={i}>{person.name}</p>
+          <p key={i}>{person.name} {person.number}</p>
         )
       })}
     </div>
