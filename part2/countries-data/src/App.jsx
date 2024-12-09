b84fd9b7b47ad39eca6572bed01562998ab66a3d
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-12-09 20:37:26 +0100

    Data for countries 2.20

--- part2/countries-data/src/App.jsx
+++ part2/countries-data/src/App.jsx
@@ -1,18 +1,23 @@
 import { useEffect, useState } from 'react'
 import axios from "axios"
+import PropTypes from 'prop-types'
 
 function App() {
 const [countries, setCountries] = useState([])
 const [search, setSearch] = useState("")
 const [selectedCountry, setSelectedCountry] = useState(null)
-const [selectedCountryInfo, setSelectedCountryInfo] = useState("")
+const [selectedCountryInfo, setSelectedCountryInfo] = useState(null)
 const [countriesToShow, setCountriesToShow] = useState([])
-
+const [weatherData, setWeatherData] = useState(null)
+const weatherAPI = import.meta.env.VITE_WEATHER_API
 useEffect(() => {
     axios.get("https://studies.cs.helsinki.fi/restcountries/api/all")
           .then((res) => {
             const countryNames = res.data.map(country => country.name.common)
             setCountries(countryNames)
+          })
+          .catch((error) => {
+            console.error("Error fetching countries:", error)
           })
   }, [])
 
@@ -22,8 +27,21 @@ useEffect(() => {
       .then(res => {
         setSelectedCountryInfo(res.data)
       })
+      .catch((error) => {
+        console.error("Error fetching country info:", error)
+      })
+
     }
   },[selectedCountry])
+
+useEffect(() => {
+  if(selectedCountryInfo){
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${selectedCountryInfo.latlng[0]}&lon=${selectedCountryInfo.latlng[1]}&appid=${weatherAPI}&units=metric`)
+      .then(res => {
+        setWeatherData(res.data)
+      })
+  }
+},[selectedCountryInfo, weatherAPI])
 
 
   const searchInputHandler = (e) => {
@@ -47,7 +65,6 @@ useEffect(() => {
   }, [countriesToShow])
 
   const handleShowButton = (name) => {
-    console.log(name.toLowerCase())
     setCountriesToShow([name.toLowerCase()])
   }
 
@@ -63,7 +80,7 @@ useEffect(() => {
       {countriesToShow.length > 10 ? (
           <p>Too many countries, specify another filter</p>
         ) : countriesToShow.length === 1 ? (
-          <Country country={selectedCountryInfo} />
+          <Country country={selectedCountryInfo} weatherData={weatherData}/>
         ) : (
           <Countries countriesToShow={countriesToShow} handleShowButton={handleShowButton} />
         )}
@@ -85,10 +102,21 @@ const Countries = ({countriesToShow, handleShowButton}) => {
   )
 }
 
-const Country = ({country}) => {
+Countries.propTypes = {
+  countriesToShow: PropTypes.arrayOf(PropTypes.string).isRequired,
+  handleShowButton: PropTypes.func.isRequired,
+}
+
+const Country = ({country, weatherData}) => {
+  console.log(weatherData)
   if (!country){
-    return null;
+    return <p>Couldnt load city</p>;
   } 
+
+  if(!weatherData){
+    return <p>Couldnt load weatherData</p>;
+  }
+
 
   return(
     <div>
@@ -97,15 +125,46 @@ const Country = ({country}) => {
       <p>Area: {country.area}</p>
       <h2>Languages</h2>
       <ul>
-      {Object.values(country.languages).map((language, index) => (
+      {country.languages && Object.values(country.languages).map((language, index) => (
           <li key={index}>{language}</li>
         ))}
       </ul>
       <br />
       <img src={country.flags.png} alt={country.flags.alt}></img>
-
+      <h2>Weather in {country.name.common}</h2>
+      <p>Temperature is: {weatherData.main.temp} Celsius</p>
+      <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt={weatherData.weather[0].description} />
+      <p>wind {weatherData.wind.speed} m/s</p>
     </div>
   )
+}
+
+Country.propTypes = {
+  country: PropTypes.shape({
+    name: PropTypes.shape({
+      official: PropTypes.string.isRequired,
+      common: PropTypes.string
+    }).isRequired,
+    capital: PropTypes.arrayOf(PropTypes.string),
+    area: PropTypes.number,
+    languages: PropTypes.objectOf(PropTypes.string),
+    flags: PropTypes.shape({
+      png: PropTypes.string,
+      alt: PropTypes.string
+    })
+  }),
+  weatherData: PropTypes.shape({
+    main: PropTypes.shape({
+      temp: PropTypes.number
+    }),
+    wind: PropTypes.shape({
+      speed: PropTypes.number
+    }),
+    weather: PropTypes.arrayOf(PropTypes.shape({
+      icon: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })).isRequired,
+  })
 }
 
 export default App
